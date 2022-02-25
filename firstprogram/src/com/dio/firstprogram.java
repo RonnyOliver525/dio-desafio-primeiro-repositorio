361a9c447d459320d0e00c12cca07e2326ecf621
RonnyOliver525 <ronny-oliver1@hotmail.com>
2022-02-24 21:46:12 -0300

    add first program in Java

---
+++ firstprogram/src/com/dio/firstprogram.java
@@ -0,0 +1,19 @@
+package com.dio;
+
+public class firstprogram {
+
+	public static void main(String[] args) {
+		
+	Cat cat = new  Cat();
+	
+	System.out.println(cat);
+
+	
+		 /*   System.out.println("Hello World!");
+		  * 
+		  */
+		// TODO Auto-generated method stub
+       
+	}
+
+}
